ca126c20136e554bdd2646b36418f559acd8ff0c
Igor <igorkomosa0@gmail.com>
2023-02-20 15:37:50 +0100

    Scaling menu

--- src/Levels/StartMenu.gd
+++ src/Levels/StartMenu.gd
@@ -5,4 +5,5 @@ export var MainGameScene : PackedScene
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
-				get_tree().change_scene(MainGameScene.resource_path)
+			get_tree().change_scene(MainGameScene.resource_path)
+	

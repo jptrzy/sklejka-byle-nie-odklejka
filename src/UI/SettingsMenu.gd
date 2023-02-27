c626c86ca11c22e080a60fbb361a35dad3c3db79
AppaTheBison21 <81151256+AppaTheBison21@users.noreply.github.com>
2023-02-27 15:28:39 +0100

    settings

--- src/UI/SettingsMenu.gd
+++ src/UI/SettingsMenu.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
 func _unhandled_input(event):
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("escape_to_pause_menu"):
 		get_tree().change_scene("res://src/UI/PauseMenu.tscn")
-	

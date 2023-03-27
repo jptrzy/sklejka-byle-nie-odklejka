9ae9d2c2734992bad02208e173b4bb115a428ae7
Igor <igorkomosa0@gmail.com>
2023-03-27 15:54:54 +0200

    Revert "Merge pull request #1 from jptrzy/igor"
    This reverts commit fd4e7b7607e85047b9975aee2a109ca24b762404, reversing
    changes made to 7f3c4ef84f2ef1c910b1a55ce4b17be2501f21a3.

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -11,17 +11,39 @@ inteligence - used for magic
 speed - movement speed, attack frequency
 """
 
+export var food := [0, 0, 0]
+
 export (int) var strength = 1
 export (int) var intelligence = 1
 export (int) var speed = 1
 
+export (int) var max_health = 1;
+var health;
+
+"""
+Movemenet
+"""
+
 var velocity = Vector2.ZERO
 
-func _physics_process(delta):
+func _ready() -> void:
+	health = max_health;
+
+func _physics_process(delta) -> void:
 	velocity = move_and_slide(velocity)
+
+func take_damage(damage: int) -> void:
+	health -= damage;
+	
+	if 0 >= health: on_death()
+
+func on_death() -> void:
+	print("TODO on death")
 
 """
 _get_property_list() don't work propertly for now,
 so the varibles can't be formated nicly in the inspector.
 For more info look at https://github.com/godotengine/godot/issues/58239
+
+WORKS IN GODOT 4
 """
--- src/Levels/StartMenu.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export var MainGameScene : PackedScene
-
-func _input(event):
-	if event is InputEventKey:
-		if event.pressed:
-			get_tree().change_scene(MainGameScene.resource_path)
-	
